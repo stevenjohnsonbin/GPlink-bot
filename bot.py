7dc3345049ea64a2c2156e3ba2349475f2b512d8
NiTRONDC1 <84825812+NiTRONDC@users.noreply.github.com>
2021-07-26 19:36:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY', '5fd20df0c4db85798dd4f5ff3d03e3606a94f98b')
+API_KEY = environ.get('API_KEY')
 
 bot = Client('gplink bot',
              api_id=API_ID,
@@ -19,7 +19,7 @@ bot = Client('gplink bot',
 async def start(bot, message):
     await message.reply(
         f"**👋Hi {message.chat.first_name}!**\n\n"
-        "I'm GPlink bot 🌚 By @ANonYmoUS_FriEND \nJust send me a link and get short link")
+        "Oops❗️ You are not Authorized to use me 😐 \n\n〓══〓══〓══〓══〓══〓══〓══〓")
 
 
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
@@ -27,7 +27,7 @@ async def link_handler(bot, message):
     link = message.matches[0].group(0)
     try:
         short_link = await get_shortlink(link)
-        await message.reply(f'Here is your [short link] \n\n({short_link})', quote=True)
+        await message.reply(f'Here is your SHORT-LiNK 🔻 \n\n{short_link}', quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
